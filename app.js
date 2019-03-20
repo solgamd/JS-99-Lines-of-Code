71f65e424dff978012a063fa748b1c4e73a6031a
solgamd <solgamd@gmail.com>
2019-03-20 11:13:34 +0100

    Got proper structure; still working on countdown

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 let friends = ['Niko','Emilie','Marj','Anna','Maria'];
-
+let i = 0;
 let howMany = 99;
 let howManyLess = howMany - 1;
+let song = [howMany + " lines of code in the file, " + howMany + " lines of code, " + friends[i] + " strikes one out, clears it all out, " + howManyLess + " lines of code in the file"];
 
-/*let song = 
-    [howMany + " lines of code in the file, "],
-    [howMany + " lines of code," ],
-    [friends[i] + " strikes one out, clears it all out, "],
-    [howManyLess + " lines of code in the file, "]
-];*/
 
 for (let i = 0; i < friends.length; i++) {
     console.log(friends[i]); 
-    let n = song.length;
-    for (let song = [howMany + " lines of code in the file, " + howMany + " lines of code, " + friends[i] + " strikes one out, clears it all out, " + howManyLess + " lines of code in the file"]; 
-            n < song.length; howMany-- ) 
-                console.log(song);
-            }
-
-
+    console.log(song);
+} while(howMany > 0) {
+    howMany--;
+}
